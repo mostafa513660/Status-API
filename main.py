fda2e6e6864083298d10cea1ff59970f6e845421
Mostafa <83466461+mostafa513660@users.noreply.github.com>
2024-01-01 15:34:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from flask import Flask, jsonify, request
+import socket
 
 app = Flask(__name__)
 
 status = True
+
+def get_host_ip():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        host_ip = s.getsockname()[0]
+        s.close()
+        return host_ip
+    except Exception as e:
+        print(f"Error getting host IP: {e}")
+        return '127.0.0.1'
 
 @app.route('/get_status', methods=['GET'])
 def get_status():
@@ -17,4 +29,4 @@ def toggle_status():
     return jsonify({'status': status})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host=host_ip, port=5000, debug=True)
